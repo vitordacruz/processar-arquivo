96ac10af690cfb3e41710873fea1a862d2e7561e
vitor <vitordacruz@gmail.com>
2022-03-29 18:18:48 -0300

    Commit Inicial

---
+++ src/main/java/com/desafio/processararquivo/ProcessarArquivoApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.processararquivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessarArquivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessarArquivoApplication.class, args);
+	}
+
+}
